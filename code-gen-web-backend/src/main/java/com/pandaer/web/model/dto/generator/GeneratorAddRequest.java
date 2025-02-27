c981edfb641aae9a94970d27509d455a93fd623a
mPandaer <liwenhaolx@gmail.com>
2025-02-27 13:15:12 +0800

    update 更新注释

--- code-gen-web-backend/src/main/java/com/pandaer/web/model/dto/generator/GeneratorAddRequest.java
+++ code-gen-web-backend/src/main/java/com/pandaer/web/model/dto/generator/GeneratorAddRequest.java
@@ -13,8 +13,7 @@ import lombok.Data;
 /**
  * 创建请求
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ 
  */
 @Data
 public class GeneratorAddRequest implements Serializable {
--- code-gen-web-backend/src/main/java/com/pandaer/web/service/GeneratorService.java
+++ code-gen-web-backend/src/main/java/com/pandaer/web/service/GeneratorService.java
@@ -15,8 +15,7 @@ import java.util.Map;
 /**
  * 帖子服务
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ 
  */
 public interface GeneratorService extends IService<Generator> {
 
